814f3aac0fea587c07432e1fb794a65cecb2a6ff
Athit <celestialwolf101@hotmail.de>
2015-10-21 12:23:24 +0700

    First commit

--- app/src/main/java/com/example/shawn/bubblenote/FileName.java
+++ app/src/main/java/com/example/shawn/bubblenote/FileName.java
@@ -4,21 +4,21 @@ public class FileName {
     private String name;
     private String shorttext;
 
-    public FileName(String Wesley, String BNote) {
-        this.name = Wesley;
-        this.shorttext = BNote;
+    public FileName(String _name, String _shorttext) {
+        this.name = _name;
+        this.shorttext = _shorttext;
     }
 
-    public void setName(String Wesley){
-        this.name = Wesley;
+    public void setName(String _name){
+        this.name = _name;
     }
 
     public String getName(){
         return this.name;
     }
 
-    public void setShorttext(String BNote){
-        this.shorttext = BNote;
+    public void setShorttext(String _shorttext){
+        this.shorttext = _shorttext;
     }
 
     public String getShorttext(){
